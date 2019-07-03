093c863a0e18311a33b6f2ee4c99ad4739d6adf1
Mikhail f. Shiryaev <mr.felixoid@gmail.com>
2019-07-03 19:09:22 +0200

    Make _to_dict() a non-static method for easier usage

--- zenmoney/zenobject.py
+++ zenmoney/zenobject.py
@@ -1,6 +1,29 @@
 #!/usr/bin/env python
 
 
+def to_dict(obj, classkey=None):
+    if isinstance(obj, dict):
+        data = {}
+        for (k, v) in obj.items():
+            data[k] = ZenObject._to_dict(v, classkey)
+
+        return data
+    elif hasattr(obj, '__iter__') and not isinstance(obj, str):
+        return [ZenObject._to_dict(v, classkey) for v in obj]
+    elif hasattr(obj, '__dict__'):
+        data = dict([
+            (key, ZenObject._to_dict(getattr(obj, key), classkey))
+            for key in obj.__dir__()
+            if not key.startswith('_')
+            and not callable(getattr(obj, key))
+        ])
+        if classkey is not None and hasattr(obj, '__class__'):
+            data[classkey] = obj.__class__.__name__
+        return data
+    else:
+        return obj
+
+
 class ZenObject(object):
     '''
     The common methods of ZenMoney objects
@@ -15,25 +38,5 @@ class ZenObject(object):
     def _type(self):
         return type(self).__name__
 
-    @staticmethod
-    def _to_dict(obj, classkey=None):
-        if isinstance(obj, dict):
-            data = {}
-            for (k, v) in obj.items():
-                data[k] = ZenObject._to_dict(v, classkey)
-
-            return data
-        elif hasattr(obj, '__iter__') and not isinstance(obj, str):
-            return [ZenObject._to_dict(v, classkey) for v in obj]
-        elif hasattr(obj, '__dict__'):
-            data = dict([
-                (key, ZenObject._to_dict(getattr(obj, key), classkey))
-                for key in obj.__dir__()
-                if not key.startswith('_')
-                and not callable(getattr(obj, key))
-            ])
-            if classkey is not None and hasattr(obj, '__class__'):
-                data[classkey] = obj.__class__.__name__
-            return data
-        else:
-            return obj
+    def _to_dict(self, classkey=None):
+        return to_dict(self, classkey)
