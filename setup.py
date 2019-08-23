cccf1c695b063ac391bed55ec392a874e82121e5
Mikhail f. Shiryaev <mr.felixoid@gmail.com>
2019-08-23 22:38:22 +0200

    Prepare to publish on PyPI

--- setup.py
+++ setup.py
@@ -4,21 +4,31 @@
 Setup script for zenmoney API
 '''
 
-from setuptools import setup
+from setuptools import setup, find_packages
 from zenmoney import VERSION
 
+
+with open('README.md') as f:
+    long_description = f.read()
 
 setup(
     name='zenmoney',
     version='.'.join(str(d) for d in VERSION),
     description='Library for zenmoney.ru API',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url='https://github.com/Felixoid/python-zenmoney',
     license='MIT',
     author='Mikhail f. Shiryaev',
     author_email='mr.felixoid@gmail.com',
+    install_requires=['requests'],
+    packages=find_packages(),
     classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
         'Environment :: Console',
         'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
     ],
     python_requires='>=3',
 )
